d89156793dcefd5ad1408b2a2bf26f611a326ea2
nilldiggonto <nilldiggonto@gmail.com>
2022-01-09 00:05:08 +0600

    Groupby with outerjoin

--- router/postRouter.py
+++ router/postRouter.py
@@ -2,9 +2,10 @@ from fastapi import FastAPI,Response,status,HTTPException,Depends,APIRouter
 from schema.schema import PostResponseSchema,CreatePostSchema
 from db.db_connect import get_db
 from sqlalchemy.orm import Session
-from db.models import Post
+from db.models import Post,Rate
 from typing import List, Optional
 from fastapi.param_functions import Body
+from sqlalchemy import func
 from utils import oauth2
 router = APIRouter(
     tags=["posts"]
@@ -14,10 +15,12 @@ router = APIRouter(
 @router.get('/posts')
 def all_posts(db:Session = Depends(get_db),limit:int=10,skip:int=0,search:Optional[str]=""):
     # posts = db.query(Post).all()
-    posts = db.query(Post).filter(Post.title.contains(search)).limit(limit).offset(skip).all()
-    data = [{'id':post.id,'user':post.owner.email,'title':post.title,'content':post.content,'publish':post.publish,'created':post.created_at.strftime('%Y-%m-%d %H:%M:%S')} for post in posts]
-    return {'status':'success','items':len(posts),'data':data}
-    # return posts
+    # posts = db.query(Post).filter(Post.title.contains(search)).limit(limit).offset(skip).all()
+    #--------------------Joining
+    posts_rate_query = db.query(Post,func.count(Rate.post_id).label('rates')).join(Rate,Rate.post_id == Post.id,isouter=True).group_by(Post.id).filter(Post.title.contains(search)).limit(limit).offset(skip).all()
+    data = [{'id':post.Post.id,'user':post.Post.owner.email,'title':post.Post.title,'content':post.Post.content,'publish':post.Post.publish,'rates':post.rates,'created':post.Post.created_at.strftime('%Y-%m-%d %H:%M:%S')} for post in posts_rate_query]
+    return {'status':'success','items':len(posts_rate_query),'data':data}
+    # return posts_rate_query
 
 #--creating posts without schema
 @router.post('/create/post')
@@ -41,10 +44,11 @@ def create_postSchema(request:CreatePostSchema,db:Session=Depends(get_db),curren
 #Fetching single post
 @router.get("/post/{id}")
 def postSingle(id:int,db:Session=Depends(get_db),current_user:int = Depends(oauth2.get_user)):
-    post = db.query(Post).filter(Post.id==id).first()
-    if not post:
+    # post = db.query(Post).filter(Post.id==id).first()
+    posts_rate_query = db.query(Post,func.count(Rate.post_id).label('rates')).join(Rate,Rate.post_id == Post.id,isouter=True).group_by(Post.id).filter(Post.id==id).first()
+    if not posts_rate_query:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail='no such query found')
-    return {'status':'success','post':{'title':post.title,'content':post.content}}
+    return {'status':'success','post':{'title':posts_rate_query.Post.title,'content':posts_rate_query.Post.content,'user':posts_rate_query.Post.owner.email,'rates':posts_rate_query.rates,'created':posts_rate_query.Post.created_at.strftime('%Y-%m-%d %H:%M:%S')}}
 #update post
 @router.put('/post/update/{id}')
 def updatePost(id:int,request:CreatePostSchema,db:Session = Depends(get_db),current_user:int = Depends(oauth2.get_user)):
