5ca2bdf8a0884bb8e50336acb2db6d9bd6f37030
nilldiggonto <nilldiggonto@gmail.com>
2022-01-10 01:12:49 +0600

    Cors Policy

--- app/main.py
+++ app/main.py
@@ -1,10 +1,23 @@
 from fastapi import FastAPI
-from db.db_connect import Base,engine
+# from db.db_connect import Base,engine
 from router import postRouter,userRouter,auth, voteRouter
-from utils.oauth2 import SECRET_KEY
-from config.config import settings
+# from fastapi.middleware.cors import CORSMiddleware 
+
 
 app  = FastAPI()
+
+# origins = [
+#     "https://google.com"
+# ]
+# #----CORS
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=origins,
+#     allow_credentials=True,
+#     allow_methods=["*"],
+#     allow_headers=["*"],
+# )
+#----------
 #_____________
 #
 #CREATING DB
